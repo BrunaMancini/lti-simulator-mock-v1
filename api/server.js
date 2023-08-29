7543c0411202b66d8c34a0c602d9137d6b8519a6
BrunaMancini <101213519+BrunaMancini@users.noreply.github.com>
2023-08-29 12:53:30 -0300

    double route

--- api/server.js
+++ api/server.js
@@ -11,6 +11,12 @@ server.get('/api/ltiSimulator/summary', (req, res) => {
     res.json(data);
   });
   
+server.get('/api/ltiSimulator/fullSimulationResult', (req, res) => {
+    const db = router.db;
+    const data = db.get('api.ltiSimulator.fullSimulationResult').value();
+    res.json(data);
+  });
+  
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
